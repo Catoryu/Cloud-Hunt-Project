c0a0225fab95c520f82ddcde67736f3bd5d38d36
Catoryu <catoryu.kuroneko@gmail.com>
2018-09-25 06:46:13 +0200

    MonoGame Project
    created files of MonoGame Project

---
+++ CloudHuntGame/CloudHuntGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CloudHuntGame
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new CloudHunt())
+                game.Run();
+        }
+    }
+#endif
+}
